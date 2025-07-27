71e91da723f96225826c58bb12e2df389c12f9d5
MJKING1276 <kingjhai1276@gmail.com>
2025-07-27 11:25:52 +0530

    final v2

--- app/main.py
+++ app/main.py
@@ -55,4 +55,4 @@ def health_check():
 
 if __name__== "__main__":
     port=int(os.environ.get("PORT", 8080))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port,)
+    uvicorn.run(app, host="0.0.0.0", port=port,)
